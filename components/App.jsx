f879771087240bae5eb8f3771cad2c8117125e63
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-06-10 11:18:26 -0700

    adding user scaffolding

--- components/App.jsx
+++ components/App.jsx
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import ChannelSection from './channels/ChannelSection.jsx';
+import UserSection from './users/UserSection.jsx';
 
 class App extends Component{
   constructor(props){
     super(props);
     this.state = {
       channels: [],
-      activeChannel: {}
+      activeChannel: {},
+      users: [],
+      activeUser: {}
     };
   }
+
   addChannel(name){
     let {channels}  = this.state;
     channels.push({id: channels.length, name});
@@ -22,6 +26,19 @@ class App extends Component{
 
   }
 
+  addUser(name){
+    let {users}  = this.state;
+    users.push({id: users.length, name});
+    this.setState({users});
+    //Todo - send to server
+  }
+
+  setUser(activeUser){
+    this.setState({activeUser});
+    // todo get User messages
+
+  }
+
   render(){
     return(
       <div className='app'>
@@ -30,6 +47,11 @@ class App extends Component{
             {...this.state}
             addChannel= {this.addChannel.bind(this)}
             setChannel= {this.setChannel.bind(this)}
+          />
+          <UserSection
+            {...this.state}
+            addUser= {this.addUser.bind(this)}
+            setUser= {this.setUser.bind(this)}
           />
         </div>
       </div>
